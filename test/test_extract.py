3b9898d05bfd77ce0112abb3428c836062b4d846
ellweaver <elliotweaver247@gmail.com>
2025-10-23 13:05:53 +0100

    create mock response fixture

---
+++ test/test_extract.py
@@ -0,0 +1,128 @@
+
+import pytest 
+import requests 
+from unittest.mock import Mock, patch
+
+@pytest.fixture
+def mock_response(monkeypatch):
+    def mock_get(*args, **kwargs):
+        def mock_json():
+            return 
+    {
+    "job_id": "t-AKAI-wU29Jup6MAAAAAA==",
+    "employer_name": "Intetics",
+    "employer_logo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK_KU_MWJwgob7h3oWQdO6HZgRFDKAYKbnACq0&s=0",
+    "employer_website": "http://intetics.com",
+    "employer_company_type": "Computer Services",
+    "employer_linkedin": null,
+    "job_publisher": "LinkedIn",
+    "job_employment_type": "FULLTIME",
+    "job_title": "Senior Node.js Developer",
+    "job_apply_link": "https://www.linkedin.com/jobs/view/senior-node-js-developer-at-intetics-3967661687?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic",
+    "job_apply_is_direct": false,
+    "job_apply_quality_score": 0.7454,
+    "apply_options": [
+        {
+            "publisher": "LinkedIn",
+            "apply_link": "https://www.linkedin.com/jobs/view/senior-node-js-developer-at-intetics-3967661687?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic",
+            "is_direct": false
+        },
+        {
+            "publisher": "Monster",
+            "apply_link": "https://www.monster.com/job-openings/senior-node-js-developer-new-york-ny--bdd8665b-7af1-48fa-9cbd-88a764d91ca1?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic",
+            "is_direct": true
+        },
+        {
+            "publisher": "ZipRecruiter",
+            "apply_link": "https://www.ziprecruiter.com/c/Intetics/Job/Senior-Node.js-Developer/-in-New-York,NY?jid=04a787e4874c47d0&utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic",
+            "is_direct": true
+        },
+        {
+            "publisher": "Jooble",
+            "apply_link": "https://jooble.org/jdp/8101226089153585842?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic",
+            "is_direct": false
+        },
+        {
+            "publisher": "Talent.com",
+            "apply_link": "https://www.talent.com/view?id=15568f3d3912&utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic",
+            "is_direct": true
+        },
+        {
+            "publisher": "BeBee",
+            "apply_link": "https://us.bebee.com/job/2410a78e0c690d84b6d5ff0c495ac7b7?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic",
+            "is_direct": false
+        },
+        {
+            "publisher": "Ladders",
+            "apply_link": "https://www.theladders.com/job/senior-node-js-developer-intetics-new-york-ny_73750565?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic",
+            "is_direct": false
+        },
+        {
+            "publisher": "Adzuna",
+            "apply_link": "https://www.adzuna.com/details/4767428727?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic",
+            "is_direct": true
+        }
+    ],
+    "job_description": "Intetics Inc., a leading global technology company providing custom software application development, distributed professional teams, software product quality assessment, and \"all-things-digital\" solutions, is looking for a Senior/Lead Node.js Developer to enrich its team with a skilled professional to spread the company's ideas, vision, content, and messages.\n\nOur client creates digital toys and other playful products for kids around the world. Since the first product launch in 2011, they have released 40+ apps that have been downloaded more than 444 million times in 238 markets, making them the No. 1 mobile-first kids brand in the App Store.\n\nRequirements\n• Great gaming development history\n• Experience with JavaScript\n• Extensive experience with Node.js\n• Knowledge of AWS microservices\n\nResponsibilities:\n• New features development\n• Development of matchmaking service\n• Working on the identity service",
+    "job_is_remote": false,
+    "job_posted_at_timestamp": 1720182992,
+    "job_posted_at_datetime_utc": "2024-07-05T12:36:32.000Z",
+    "job_city": "New York",
+    "job_state": "NY",
+    "job_country": "US",
+    "job_latitude": 40.712776,
+    "job_longitude": -74.005974,
+    "job_benefits": null,
+    "job_google_link": "https://www.google.com/search?gl=us&hl=en&rciv=jb&q=node.js+developer+in+new-york,usa&start=0&udm=8#vhid=vt%3D20/docid%3Dt-AKAI-wU29Jup6MAAAAAA%3D%3D&vssid=jobs-detail-viewer",
+    "job_offer_expiration_datetime_utc": "2025-01-01T12:47:25.000Z",
+    "job_offer_expiration_timestamp": 1735735645,
+    "job_required_experience": {
+        "no_experience_required": "false",
+        "required_experience_in_months": null,
+        "experience_mentioned": "true",
+        "experience_preferred": "false"
+    },
+    "job_required_skills": null,
+    "job_required_education": {
+        "postgraduate_degree": "false",
+        "professional_certification": "false",
+        "high_school": "false",
+        "associates_degree": "false",
+        "bachelors_degree": "true",
+        "degree_mentioned": "false",
+        "degree_preferred": "false",
+        "professional_certification_mentioned": "false"
+    },
+    "job_experience_in_place_of_education": false,
+    "job_min_salary": null,
+    "job_max_salary": null,
+    "job_salary_currency": null,
+    "job_salary_period": null,
+    "job_highlights": {
+        "Qualifications": [
+            "Great gaming development history",
+            "Experience with JavaScript",
+            "Extensive experience with Node.js",
+            "Knowledge of AWS microservices"
+        ],
+        "Responsibilities": [
+            "Development of matchmaking service",
+            "Working on the identity service"
+        ]
+    },
+    "job_job_title": "Senior",
+    "job_posting_language": "en",
+    "job_onet_soc": "15113400",
+    "job_onet_job_zone": "3",
+    "job_occupational_categories": null,
+    "job_naics_code": "541511",
+    "job_naics_name": "Custom Computer Programming Services"
+}
+            
+
+        mock = Mock(spec=requests.get)
+        mock.status_code = 200
+        mock.json = mock_json
+        return mock
+
+    monkeypatch.setattr(requests, "get", mock_get)
