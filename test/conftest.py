ab37e2ec7b49a98e22d455d39056b4386a3db938
ellweaver <elliotweaver247@gmail.com>
2025-10-23 14:10:07 +0100

    create fixture file

---
+++ test/conftest.py
@@ -0,0 +1,21 @@
+from json.json_example import ninja_response
+import pytest 
+import requests 
+from unittest.mock import Mock
+
+@pytest.fixture
+def mock_response(monkeypatch):
+    def mock_get(*args, **kwargs):
+        def mock_json():
+            return ninja_response
+    
+            
+
+        mock = Mock(spec=requests.get)
+        mock.status_code = 200
+        mock.json = mock_json
+        return mock
+
+    monkeypatch.setattr(requests, "get", mock_get)
+
+
