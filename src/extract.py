226ff0137750f1bb065016fca5fa3fcba673581b
ellweaver <elliotweaver247@gmail.com>
2025-10-23 15:41:11 +0100

    update event load

--- src/extract.py
+++ src/extract.py
@@ -1,11 +1,19 @@
 import requests
 import os
 from dotenv import load_dotenv
+from pprint import pprint 
+import json
+load_dotenv()
 
-def extract(event=os.getenv["DEFAULT_EVENT"]):
-    
+def extract(event=os.getenv("DEFAULT_EVENT")):
+    print(event)
+    event=json.loads(event) 
     params=event["params"]
     headers = event["api_key"]
+    print(headers)
     url = event["url"]
     response=requests.get(url,   params=params, headers=headers)
     return response
+
+if __name__ =="__main__":
+    pprint(extract())
