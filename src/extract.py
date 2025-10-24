0981f04415f88a7e26621d512fecba99099c5c8d
Seb <seb.allen@protonmail.com>
2025-10-24 14:21:45 +0100

    improve readability

--- src/extract.py
+++ src/extract.py
@@ -3,14 +3,15 @@ import os
 from dotenv import load_dotenv
 from pprint import pprint 
 import json
+
 load_dotenv()
 
 def extract(event=os.getenv("DEFAULT_EVENT")):
-    event=json.loads(event) 
-    params=event["params"]
+    event = json.loads(event) 
+    params = event["params"]
     headers = event["api_key"]
     url = event["url"]
-    response=requests.get(url, headers=headers, params=params)
+    response = requests.get(url, headers=headers, params=params)
     return response
 
 if __name__ =="__main__":
