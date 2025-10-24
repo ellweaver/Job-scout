e5bba1587b28af10b17a50339373b91a6c5e600f
Seb <seb.allen@protonmail.com>
2025-10-24 14:24:29 +0100

    assert request.get is called with correct arguments

--- test/test_extract.py
+++ test/test_extract.py
@@ -5,8 +5,36 @@ from unittest.mock import Mock
 from src.extract import extract
 
 class TestExtract:
-    @pytest.mark.it('extractfunction returns string value')
+    @pytest.mark.it('extract function returns string value')
     def test_extract_string(self, mock_response):
-        assert isinstance(extract(), str)
+        assert isinstance(extract().json(), dict)
+        assert isinstance(extract().status_code, int)
 
-    
+    @pytest.mark.it('extract function is called with the correct arguments')
+    def test_extract_arguments(self, monkeypatch):
+        event = """{
+            "url":"https://api.openwebninja.com/jsearch/search",
+            "api_key": {"x-api-key":"TEST KEY"},
+            "params":{"query":"junior python"}
+            }"""
+        
+        class MiniMockResponse(Mock):
+            @staticmethod
+            def json():
+                return ninja_response
+            
+            @staticmethod
+            def status_code():
+                return 200
+            
+        mock = MiniMockResponse()
+
+        monkeypatch.setattr(requests, "get", mock)
+
+        extract(event)
+
+        mock.assert_called_with(
+            "https://api.openwebninja.com/jsearch/search",
+            headers={"x-api-key":"TEST KEY"},
+            params={"query":"junior python"}
+        )
