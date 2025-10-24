5dee29c6b2b704e53352f38782b8d69d8d28716e
Seb <seb.allen@protonmail.com>
2025-10-24 14:22:29 +0100

    create MockResponse Class

--- test/conftest.py
+++ test/conftest.py
@@ -3,19 +3,23 @@ import pytest
 import requests 
 from unittest.mock import Mock
 
-@pytest.fixture
+
+class MockResponse(Mock):
+    @staticmethod
+    def json():
+        return ninja_response
+    
+    @staticmethod
+    def status_code():
+        return 200
+    
+    def __init__(self, spec = None, side_effect = None, return_value = ..., wraps = None, name = None, spec_set = None, parent = None, _spec_state = None, _new_name = "", _new_parent = None, **kwargs):
+         super().__init__(spec, side_effect, return_value, wraps, name, spec_set, parent, _spec_state, _new_name, _new_parent, **kwargs)
+         self.status_code = 200
+
+@pytest.fixture 
 def mock_response(monkeypatch):
     def mock_get(*args, **kwargs):
-        def mock_json():
-            return ninja_response
-    
-            
-
-        mock = Mock(spec=requests.get)
-        mock.status_code = 200
-        mock.json = mock_json
-        return mock
+            return MockResponse()
 
     monkeypatch.setattr(requests, "get", mock_get)
-
-
