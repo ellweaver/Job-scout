a3a84aa70d93d061f5f9c83be5601bdb47f8446e
ellweaver <elliotweaver247@gmail.com>
2025-10-27 13:04:13 +0000

    fix json.load

--- src/search.py
+++ src/search.py
@@ -13,9 +13,9 @@ def perform_search(filepath="./json_files/default_event.json", api_key=os.getenv
     Turns Json File into event string and inputs string into extract function.
     """
     with open(filepath) as f:
-        event= json.dump(f)
+        event= json.load(f)   
     event["api_key"] = api_key
-    json.dumps(event)
+    event=json.dumps(event)
     response=extract(event)
     return response
     
