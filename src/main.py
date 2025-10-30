ad7df36b1870066267bc502a926dfca1da9fcbc7
Seb <seb.allen@protonmail.com>
2025-10-30 14:43:46 +0000

    initial commit

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from src.search import generate_search_file, perform_search
+
+
+
+
+def main():
+    """
+    queries whether we want to immediately perform a search, calling a file or using existing defaults
+    or
+    generate search
+    """
+    pass
