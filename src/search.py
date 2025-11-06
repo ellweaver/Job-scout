aa0cd4728992768fdafc290703443b2e9d06c345
Seb <seb.allen@protonmail.com>
2025-11-06 12:01:02 +0000

    ran black

--- src/search.py
+++ src/search.py
@@ -7,19 +7,17 @@ from datetime import datetime
 load_dotenv()
 
 
-
-
 def perform_search(
     search_directory="./search_queries/",
     search_filename="default_search.json",
     api_key=os.getenv("API_KEY"),
-    destination_directory="./search_results/"
+    destination_directory="./search_results/",
 ):
     """
     Turns Json File into event string and inputs string into extract function.
     """
     search_filepath = search_directory + search_filename
-    
+
     with open(search_filepath) as f:
         event = json.load(f)
 
@@ -27,12 +25,15 @@ def perform_search(
 
     response = extract(event)
     destination_timestamp = str(datetime.now())
-    destination_filepath = destination_directory + destination_timestamp + " " + search_filename
-    
+    destination_filepath = (
+        destination_directory + destination_timestamp + " " + search_filename
+    )
+
     with open(destination_filepath, "w") as f:
         f.write(json.dumps(response.json(), indent=4))
 
     return response
+
 
 def manual_search():
     """_summary_
@@ -44,21 +45,28 @@ def manual_search():
     - api token
     """
 
-    search_directory = input("Please enter the directory for your search file [./search_queries/]: ").strip()
-    search_filename = input("Please enter the filename of your search file [default_search.json]: ").strip()
-    destination_directory = input("Please enter an existing save location for your query [./search_results/]: ").strip()
+    search_directory = input(
+        "Please enter the directory for your search file [./search_queries/]: "
+    ).strip()
+    search_filename = input(
+        "Please enter the filename of your search file [default_search.json]: "
+    ).strip()
+    destination_directory = input(
+        "Please enter an existing save location for your query [./search_results/]: "
+    ).strip()
     api_token = input("Please enter your OpenWeb Ninja API token [.env]: ").strip()
 
     params = {
         "search_directory": search_directory,
         "search_filename": search_filename,
         "destination_directory": destination_directory,
-        "api_token": api_token
+        "api_token": api_token,
     }
 
-    not_none_params = {k:v for k, v in params.items() if v}
+    not_none_params = {k: v for k, v in params.items() if v}
 
-    return perform_search(**not_none_params )
+    return perform_search(**not_none_params)
+
 
 def generate_search_file(
     url="https://api.openwebninja.com/jsearch/search",
@@ -76,60 +84,64 @@ def generate_search_file(
     radius="25",
     exclude_job_publishers="",
     fields="",
-    advanced=False
+    advanced=False,
 ):
     """
     Console tool to create a json file which can be used for perform_search also returns event dict
     """
-    
+
     while not query:
         query = input("Enter your search Query: ")
-    
+
     if advanced == True:
 
         user_response = input("page [1]: ").strip()
         if user_response:
-            page=user_response
+            page = user_response
 
         user_response = input("number of pages [1]: ").strip()
         if user_response:
-            num_pages=user_response
+            num_pages = user_response
 
         user_response = input("country [gb]: ").strip()
         if user_response:
-            country=user_response
+            country = user_response
 
         user_response = input("language [en]: ").strip()
         if user_response:
-            language=user_response
+            language = user_response
 
         user_response = input("date posted [all]: ").strip()
         if user_response:
-            date_posted=user_response
+            date_posted = user_response
 
-        user_response = input("work from home [False]: ").strip().lower().startswith('t')
+        user_response = (
+            input("work from home [False]: ").strip().lower().startswith("t")
+        )
         if user_response:
-            work_from_home=user_response
+            work_from_home = user_response
 
-        user_response=input('employment types ["FULLTIME", "CONTRACTOR", "PARTTIME", "INTERN"]: ').strip()
+        user_response = input(
+            'employment types ["FULLTIME", "CONTRACTOR", "PARTTIME", "INTERN"]: '
+        ).strip()
         if user_response:
-            employment_types=user_response
+            employment_types = user_response
 
         user_response = input("job requirements []: ").strip()
         if user_response:
-            job_requirements=user_response
+            job_requirements = user_response
 
         user_response = input("radius [25]: ").strip()
         if user_response:
-            radius=user_response
+            radius = user_response
 
         user_response = input("exclude job publishers []: ").strip()
         if user_response:
-            exclude_job_publishers=user_response
+            exclude_job_publishers = user_response
 
         user_response = input("fields []: ").strip()
         if user_response:
-            fields=user_response
+            fields = user_response
 
     params = {
         "query": query,
@@ -143,35 +155,62 @@ def generate_search_file(
         "job_requirements": job_requirements,
         "radius": radius,
         "exclude_job_publishers": exclude_job_publishers,
-        "fields": fields
-        }
-
-    not_none_params = {k:v for k, v in params.items() if v is not "" and v is not []}
-
-    event = {
-        "api_key": {},
-        "url": url,
-        "params": not_none_params
+        "fields": fields,
     }
-    
+
+    not_none_params = {k: v for k, v in params.items() if v != "" and v != []}
+
+    event = {"api_key": {}, "url": url, "params": not_none_params}
+
     while not file_name:
-        user_response = input(f"Please enter your Search file name without extension[{query}]: ").lstrip()
-        if  user_response:
+        user_response = input(
+            f"Please enter your Search file name without extension[{query}]: "
+        ).lstrip()
+        if user_response:
             file_name = user_response
         else:
             file_name = query
 
-    
     file_name = file_name + ".json"
-        
+
     search_filepath = search_directory + file_name
-    
+
     with open(search_filepath, "w") as f:
         f.write(json.dumps(event, indent=4))
 
-    return {"event": event, "search_directory": search_directory, "filename": file_name, "filepath": search_filepath}
+    return {
+        "event": event,
+        "search_directory": search_directory,
+        "filename": file_name,
+        "filepath": search_filepath,
+    }
+
 
 def list_search_directory(search_directory="search_queries/"):
-    """lists all files in specified directory returns file path chosen by user input check file has url and params keys and query key in params """
+    """
+    lists all files in specified directory returns file path chosen by
+    user input check file has url and params keys and query key in params
+    """
 
+    search_files = {}
 
+    for i, file in enumerate(os.listdir(search_directory)):
+        if file.endswith(".json"):
+            search_files[i] = file
+    
+    for k, v in search_files.items():
+        print(f"[{k}] - {v}")
+
+    file_name = None
+
+    while file_name == None:
+        user_input = int(input("Please enter a number that corresponds with a search file: "))
+        file_name = search_files.get(user_input)
+
+    filepath = search_directory + file_name
+
+    return {
+        'filepath': filepath,
+        "search_directory": search_directory,
+        'filename': file_name
+        }
--- test/test_search.py
+++ test/test_search.py
@@ -6,8 +6,9 @@ import pytest
 from freezegun import freeze_time
 from datetime import datetime
 
+
 class TestPerformSearch:
-    
+
     @freeze_time("2025-01-31")
     @pytest.mark.it("extract called with correct event")
     def test_search_calls_extract_correctly(self, mock_response, monkeypatch):
@@ -25,34 +26,62 @@ class TestPerformSearch:
         monkeypatch.setattr("src.search.extract", mock)
 
         test_api_token = "TEST_API"
-        test_search_results_directory="./test/test_json/"
+        test_search_results_directory = "./test/test_json/"
 
         user_query = "test query in london"
         file_name = "test_search_query"
 
-        test_event = {"api_key": {"x-api-key": "TEST_API"}, "url": "https://api.openwebninja.com/jsearch/search", "params": {"query": "test query in london", "page": "1", "num_pages": "1", "country": "gb", "language": "en", "date_posted": "all", "work_from_home": False, "employment_types": "FULLTIME,CONTRACTOR,PARTTIME,INTERN", "radius": "25"}}
-        
-        filepath = generate_search_file(query=user_query, file_name=file_name, search_directory="./test/test_json/")
-        search_directory= filepath["search_directory"]
-        search_filename=filepath["filename"]
-        perform_search(search_directory,search_filename, test_api_token, test_search_results_directory)
+        test_event = {
+            "api_key": {"x-api-key": "TEST_API"},
+            "url": "https://api.openwebninja.com/jsearch/search",
+            "params": {
+                "query": "test query in london",
+                "page": "1",
+                "num_pages": "1",
+                "country": "gb",
+                "language": "en",
+                "date_posted": "all",
+                "work_from_home": False,
+                "employment_types": "FULLTIME,CONTRACTOR,PARTTIME,INTERN",
+                "radius": "25",
+            },
+        }
+
+        filepath = generate_search_file(
+            query=user_query, file_name=file_name, search_directory="./test/test_json/"
+        )
+        search_directory = filepath["search_directory"]
+        search_filename = filepath["filename"]
+        perform_search(
+            search_directory,
+            search_filename,
+            test_api_token,
+            test_search_results_directory,
+        )
 
         mock.assert_called_with(test_event)
-    
+
     @freeze_time("2025-01-31")
     @pytest.mark.it("perform search returns correct response")
     def test_search_returns_correct_response(self, mock_response):
         test_api_token = "TEST_API"
-        test_search_results_directory="./test/test_json/"
+        test_search_results_directory = "./test/test_json/"
 
         user_query = "test query in london"
         file_name = "test_search_query"
 
-        filepath = generate_search_file(query=user_query, file_name=file_name, search_directory="./test/test_json/")
-        search_directory= filepath["search_directory"]
-        search_filename=filepath["filename"]
+        filepath = generate_search_file(
+            query=user_query, file_name=file_name, search_directory="./test/test_json/"
+        )
+        search_directory = filepath["search_directory"]
+        search_filename = filepath["filename"]
 
-        response = perform_search(search_directory, search_filename, test_api_token, test_search_results_directory)
+        response = perform_search(
+            search_directory,
+            search_filename,
+            test_api_token,
+            test_search_results_directory,
+        )
 
         assert isinstance(response.json(), dict)
         assert isinstance(response.status_code, int)
@@ -64,56 +93,92 @@ class TestPerformSearch:
         user_query = "test query in Jamaica"
         test_api_token = "TEST_API"
         destination_directory = "./test/test_json/"
-        destination_timestamp=str(datetime.now())
-        monkeypatch.setattr('builtins.input', lambda _:user_filename)
-        
-        search_filepath = generate_search_file(query=user_query, search_directory="./test/test_json/")
-        search_directory= search_filepath["search_directory"]
-        search_filename=search_filepath["filename"]
+        destination_timestamp = str(datetime.now())
+        monkeypatch.setattr("builtins.input", lambda _: user_filename)
+
+        search_filepath = generate_search_file(
+            query=user_query, search_directory="./test/test_json/"
+        )
+        search_directory = search_filepath["search_directory"]
+        search_filename = search_filepath["filename"]
 
         perform_search(
             search_directory=search_directory,
             search_filename=search_filename,
             api_key=test_api_token,
-            destination_directory=destination_directory)
+            destination_directory=destination_directory,
+        )
 
-        destination_filepath=destination_directory+destination_timestamp+" "+user_filename+".json"
+        destination_filepath = (
+            destination_directory
+            + destination_timestamp
+            + " "
+            + user_filename
+            + ".json"
+        )
         with open(destination_filepath, "r") as f:
             results_file = f.read()
-        
+
         assert json.loads(results_file) == ninja_response
 
 
 class TestGenerateSearchFile:
-    @pytest.mark.it('Generate_search_file returns dictionary')
-    def test_generate_search_file_dict(self,monkeypatch):
-        monkeypatch.setattr('builtins.input', lambda _:"test query in london")
-        assert isinstance(generate_search_file(search_directory="./test/test_json/" ,file_name="test_search_query"), dict)
-    
-    @pytest.mark.it('Generate_search_file returns correct result')
+    @pytest.mark.it("Generate_search_file returns dictionary")
+    def test_generate_search_file_dict(self, monkeypatch):
+        monkeypatch.setattr("builtins.input", lambda _: "test query in london")
+        assert isinstance(
+            generate_search_file(
+                search_directory="./test/test_json/", file_name="test_search_query"
+            ),
+            dict,
+        )
+
+    @pytest.mark.it("Generate_search_file returns correct result")
     def test_Generate_search_file_result(self, monkeypatch):
-        user_query="test query in london"
-        monkeypatch.setattr('builtins.input', lambda _:user_query)
-        test_event={
-        "api_key": {},
-        "url": "https://api.openwebninja.com/jsearch/search",
-        "params": {"query": "test query in london", "page":"1", "num_pages":"1", "country":"gb","language":"en","date_posted":"all","work_from_home":False,"employment_types":"FULLTIME,CONTRACTOR,PARTTIME,INTERN","radius":"25"}
-    }
-        assert generate_search_file(search_directory="./test/test_json/", file_name="test_search_query")["event"] == test_event
+        user_query = "test query in london"
+        monkeypatch.setattr("builtins.input", lambda _: user_query)
+        test_event = {
+            "api_key": {},
+            "url": "https://api.openwebninja.com/jsearch/search",
+            "params": {
+                "query": "test query in london",
+                "page": "1",
+                "num_pages": "1",
+                "country": "gb",
+                "language": "en",
+                "date_posted": "all",
+                "work_from_home": False,
+                "employment_types": "FULLTIME,CONTRACTOR,PARTTIME,INTERN",
+                "radius": "25",
+            },
+        }
+        assert (
+            generate_search_file(
+                search_directory="./test/test_json/", file_name="test_search_query"
+            )["event"]
+            == test_event
+        )
 
-    @pytest.mark.it('Generate_search_file returns correct filepath from user input')
-    def test_generate_search_file_filepath(self,monkeypatch):
-        user_filename="test_search_query"
-        user_query="test query in london"
-        monkeypatch.setattr('builtins.input', lambda _:user_filename)
-        assert generate_search_file(query=user_query, search_directory="./test/test_json/")["filepath"] =="./test/test_json/test_search_query.json"
+    @pytest.mark.it("Generate_search_file returns correct filepath from user input")
+    def test_generate_search_file_filepath(self, monkeypatch):
+        user_filename = "test_search_query"
+        user_query = "test query in london"
+        monkeypatch.setattr("builtins.input", lambda _: user_filename)
+        assert (
+            generate_search_file(
+                query=user_query, search_directory="./test/test_json/"
+            )["filepath"]
+            == "./test/test_json/test_search_query.json"
+        )
 
-    @pytest.mark.it('Test that generated search parameter file saves in expected location')
+    @pytest.mark.it(
+        "Test that generated search parameter file saves in expected location"
+    )
     def test_file_save(self, monkeypatch):
         user_filename = "test_search_query"
         user_query = "test query in Jamaica"
-        monkeypatch.setattr('builtins.input', lambda _:user_filename)
-        test_file="""{
+        monkeypatch.setattr("builtins.input", lambda _: user_filename)
+        test_file = """{
     "api_key": {},
     "url": "https://api.openwebninja.com/jsearch/search",
     "params": {
@@ -128,55 +193,85 @@ class TestGenerateSearchFile:
         "radius": "25"
     }
 }"""
-        
-        filepath = generate_search_file(query=user_query, search_directory="./test/test_json/")['filepath']
+
+        filepath = generate_search_file(
+            query=user_query, search_directory="./test/test_json/"
+        )["filepath"]
 
         with open(filepath, "r") as f:
             search_file = f.read()
 
         assert search_file == test_file
 
-    @pytest.mark.it('generate search file runs advanced checks with inputs and generates params correctly')
-    def test_generate_advanced (self, monkeypatch):
-        inputs= iter(["test query in Japan", "2", "2", "us", "de", "all", "True","FULLTIME,CONTRACTOR,INTERN", "no_degree", "15", "beeBe,Dice", "employer_name,job_publisher,job_title,job_country" ])
-        monkeypatch.setattr('builtins.input', lambda _:next(inputs))
-        response = generate_search_file(advanced=True, file_name="test_search_query",search_directory="./test/test_json/")
-        assert response["event"]["params"]=={
-        "query": "test query in Japan",
-        "page": "2",
-        "num_pages": "2",
-        "country": "us",
-        "language": "de",
-        "date_posted": "all",
-        "work_from_home": True,
-        "employment_types": "FULLTIME,CONTRACTOR,INTERN",
-        "job_requirements": "no_degree",
-        "radius": "15",
-        "exclude_job_publishers": "beeBe,Dice",
-        "fields": "employer_name,job_publisher,job_title,job_country"
+    @pytest.mark.it(
+        "generate search file runs advanced checks with inputs and generates params correctly"
+    )
+    def test_generate_advanced(self, monkeypatch):
+        inputs = iter(
+            [
+                "test query in Japan",
+                "2",
+                "2",
+                "us",
+                "de",
+                "all",
+                "True",
+                "FULLTIME,CONTRACTOR,INTERN",
+                "no_degree",
+                "15",
+                "beeBe,Dice",
+                "employer_name,job_publisher,job_title,job_country",
+            ]
+        )
+        monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+        response = generate_search_file(
+            advanced=True,
+            file_name="test_search_query",
+            search_directory="./test/test_json/",
+        )
+        assert response["event"]["params"] == {
+            "query": "test query in Japan",
+            "page": "2",
+            "num_pages": "2",
+            "country": "us",
+            "language": "de",
+            "date_posted": "all",
+            "work_from_home": True,
+            "employment_types": "FULLTIME,CONTRACTOR,INTERN",
+            "job_requirements": "no_degree",
+            "radius": "15",
+            "exclude_job_publishers": "beeBe,Dice",
+            "fields": "employer_name,job_publisher,job_title,job_country",
         }
 
-    @pytest.mark.it('Generate search file uses default params when input is empty')
-    def test_generate_advanced_empty_params (self, monkeypatch):
-        inputs= iter(["test_search_query", "", "", "", "", "", "","", "", "", "", "", "" ])
-        monkeypatch.setattr('builtins.input', lambda _:next(inputs))
-        response = generate_search_file(advanced=True, search_directory="./test/test_json/")
-        assert response["event"]["params"]=={
-        "query": "test_search_query",
-        "page": "1",
-        "num_pages": "1",
-        "country": "gb",
-        "language": "en",
-        "date_posted": "all",
-        "work_from_home": False,
-        "employment_types": "FULLTIME,CONTRACTOR,PARTTIME,INTERN",
-        "radius": "25",
+    @pytest.mark.it("Generate search file uses default params when input is empty")
+    def test_generate_advanced_empty_params(self, monkeypatch):
+        inputs = iter(
+            ["test_search_query", "", "", "", "", "", "", "", "", "", "", "", ""]
+        )
+        monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+        response = generate_search_file(
+            advanced=True, search_directory="./test/test_json/"
+        )
+        assert response["event"]["params"] == {
+            "query": "test_search_query",
+            "page": "1",
+            "num_pages": "1",
+            "country": "gb",
+            "language": "en",
+            "date_posted": "all",
+            "work_from_home": False,
+            "employment_types": "FULLTIME,CONTRACTOR,PARTTIME,INTERN",
+            "radius": "25",
         }
         assert response["filename"] == "test_search_query.json"
 
+
 class TestManualSearch:
-    @pytest.mark.it('manual search passes user input correctly to perform search function')
-    def test_manual_pass_input(self,monkeypatch, mock_response):
+    @pytest.mark.it(
+        "manual search passes user input correctly to perform search function"
+    )
+    def test_manual_pass_input(self, monkeypatch, mock_response):
         class MiniMockResponse(Mock):
             @staticmethod
             def json():
@@ -189,14 +284,28 @@ class TestManualSearch:
         mock = MiniMockResponse()
 
         monkeypatch.setattr("src.search.perform_search", mock)
-        inputs= iter(["./test/test_json/","test_search_query.json","./test/test_json/", "test-api"])
-        monkeypatch.setattr('builtins.input', lambda _:next(inputs))
+        inputs = iter(
+            [
+                "./test/test_json/",
+                "test_search_query.json",
+                "./test/test_json/",
+                "test-api",
+            ]
+        )
+        monkeypatch.setattr("builtins.input", lambda _: next(inputs))
 
         manual_search()
-        mock.assert_called_with(search_directory='./test/test_json/', search_filename='test_search_query.json', destination_directory='./test/test_json/', api_token='test-api')
+        mock.assert_called_with(
+            search_directory="./test/test_json/",
+            search_filename="test_search_query.json",
+            destination_directory="./test/test_json/",
+            api_token="test-api",
+        )
 
-    @pytest.mark.it('Manual search passes default values to perform serarch function when user does not input response')
-    def test_manual_pass_default(self,monkeypatch, mock_response):
+    @pytest.mark.it(
+        "Manual search passes default values to perform serarch function when user does not input response"
+    )
+    def test_manual_pass_default(self, monkeypatch, mock_response):
         class MiniMockResponse(Mock):
             @staticmethod
             def json():
@@ -208,18 +317,19 @@ class TestManualSearch:
 
         mock = MiniMockResponse()
 
-
         monkeypatch.setattr("src.search.perform_search", mock)
-        inputs= iter(["","","", ""])
-        monkeypatch.setattr('builtins.input', lambda _:next(inputs))
+        inputs = iter(["", "", "", ""])
+        monkeypatch.setattr("builtins.input", lambda _: next(inputs))
 
         manual_search()
         mock.assert_called_with()
 
+
 class TestListSearchDirectory:
-    @pytest.mark.it('returns correct filepath with correct user input')
+    @pytest.mark.it("returns correct filepath with correct user input")
     def test_list_returns_correct_file_path(self):
         pass
-    @pytest.mark.it('rejects file if not valid search Json')
+
+    @pytest.mark.it("rejects file if not valid search Json")
     def test_list_validates_non_search_files(self):
         pass
